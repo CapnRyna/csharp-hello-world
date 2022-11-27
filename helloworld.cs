2ad0602aee4dc7c45488599b5e750a06c554c0ac
CapRyna <100812735+CapnRyna@users.noreply.github.com>
2022-11-27 14:17:05 -0500

    Create helloworld.cs

---
+++ helloworld.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace helloWorld
+{
+
+    class helloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
