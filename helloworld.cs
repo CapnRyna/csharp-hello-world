9e4fb033f8770b62560e36fb76c49aa95f6fe0bb
CapRyna <100812735+CapnRyna@users.noreply.github.com>
2022-11-27 14:17:24 -0500

    Update helloworld.cs

--- helloworld.cs
+++ helloworld.cs
@@ -1,13 +1,14 @@
 using System;
 
-namespace helloWorld
+namespace Test
 {
 
-    class helloWorld
+    class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.ReadLine();
         }
     }
 }
